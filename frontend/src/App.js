8f278ce3d72f9fa45a3951b91cc42bf2555c4f68
Amaydeveloper <amaydeveloper@gmail.com>
2023-03-08 20:08:51 +0100

    homeScreen and products added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
 
       <main className="py-3">
         <Container>
-          <h1>welcome to your eCommerce</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
