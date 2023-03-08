0ce90c6aae60bff87a9737f96068a6315591637b
Amaydeveloper <amaydeveloper@gmail.com>
2023-03-08 19:27:43 +0100

    header and footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
-  return <>welcome to your eCommerce</>;
+  return (
+    <>
+      <Header />
+
+      <main className="py-3">
+        <Container>
+          <h1>welcome to your eCommerce</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
