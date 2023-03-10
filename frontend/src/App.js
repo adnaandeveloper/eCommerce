57674c8d2ab3af056cf8ff2785bf561aa635ac7c
Amaydeveloper <amaydeveloper@gmail.com>
2023-03-10 20:12:22 +0100

    added product details schreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
           </Routes>
         </Container>
       </main>
