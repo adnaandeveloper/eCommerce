b5f2cb617e647b3835c79e3c4d8c10b93d8872c1
Amaydeveloper <amaydeveloper@gmail.com>
2023-03-16 20:13:25 +0100

    update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="m-auto">
+            <Nav className="ms-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i> Cart
