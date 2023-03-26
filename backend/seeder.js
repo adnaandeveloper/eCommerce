b805e5af7d7f178e3e2e220b1ef384a895e09c16
Amaydeveloper <amaydeveloper@gmail.com>
2023-03-26 16:53:13 +0200

    seeder added

---
+++ backend/seeder.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/users.js';
+import product from './data/products.js';
+
+import User from './models/userModel';
+import Product from './models/productModel';
+import Order from './models/OrderModel';
