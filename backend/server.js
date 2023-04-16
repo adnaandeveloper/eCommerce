95fd31a8da2e8bf28069096797184f81d53b5e57
Amaydeveloper <amaydeveloper@gmail.com>
2023-04-17 00:21:31 +0200

    middleware added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/db.js';
 import colors from 'colors';
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
@@ -16,6 +16,11 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(
