26a89e47e446dada00776b09ac65309115d09de4
Amaydeveloper <amaydeveloper@gmail.com>
2023-04-21 10:54:49 +0200

    Clean Up By Using Controllers

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,3 @@
-import { bindActionCreators } from 'redux';
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
