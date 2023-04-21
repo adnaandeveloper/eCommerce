4ffbfd7169d79dee5c230852033bc493f3ff0952
Amaydeveloper <amaydeveloper@gmail.com>
2023-04-21 18:31:56 +0200

    uyser login reducer & action added

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,21 @@
+import {
+  USER_LOGION_FAIL,
+  USER_LOGION_REQUEST,
+  USER_LOGION_SUCCESS,
+  USER_LOGOUT,
+} from '../constants/userConstants';
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_LOGION_REQUEST:
+      return { loading: true };
+    case USER_LOGION_SUCCESS:
+      return { loading: false, userInfor: action.payload };
+    case USER_LOGION_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
